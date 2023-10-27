a27e95f8acd73d28b8d398f6f4c97548e95adf3d
Emir <emirkan.ulker9@gmail.com>
2023-10-27 09:26:57 +0300

    v2

---
+++ BilgiYarismasi/BilgiYarismasi/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace BilgiYarismasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnA = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnA
+            // 
+            this.btnA.Location = new System.Drawing.Point(214, 240);
+            this.btnA.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(121, 71);
+            this.btnA.TabIndex = 0;
+            this.btnA.UseVisualStyleBackColor = true;
+            this.btnA.Click += new System.EventHandler(this.btnA_Click);
+            // 
+            // btnB
+            // 
+            this.btnB.Location = new System.Drawing.Point(544, 240);
+            this.btnB.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(121, 71);
+            this.btnB.TabIndex = 1;
+            this.btnB.UseVisualStyleBackColor = true;
+            this.btnB.Click += new System.EventHandler(this.btnB_Click);
+            // 
+            // btnC
+            // 
+            this.btnC.Location = new System.Drawing.Point(214, 402);
+            this.btnC.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(121, 71);
+            this.btnC.TabIndex = 2;
+            this.btnC.UseVisualStyleBackColor = true;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // btnD
+            // 
+            this.btnD.Location = new System.Drawing.Point(544, 402);
+            this.btnD.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(121, 71);
+            this.btnD.TabIndex = 3;
+            this.btnD.UseVisualStyleBackColor = true;
+            this.btnD.Click += new System.EventHandler(this.btnD_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(214, 21);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(449, 178);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 52);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Soru:";
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnBasla.Location = new System.Drawing.Point(34, 109);
+            this.btnBasla.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(111, 58);
+            this.btnBasla.TabIndex = 7;
+            this.btnBasla.Text = "Başlat";
+            this.btnBasla.UseVisualStyleBackColor = false;
+            this.btnBasla.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 240);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Kontrol";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 292);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Cevap";
+            this.label4.Visible = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(707, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Puan Tablosu";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(34, 60);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 20);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Yanlış:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(93, 60);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(16, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 31);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Doğru:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(93, 31);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(703, 147);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(42, 20);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Süre:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(745, 147);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(23, 20);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "20";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(926, 503);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnBasla);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnA);
+            this.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Bilgi Yarışması";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ BilgiYarismasi/BilgiYarismasi/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BilgiYarismasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int soruNo = 0;
+        int dogruCevap = 0;
+        int yanlisCevap = 0;
+
+        int sure=20;
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            btnBasla.Enabled = false;
+            timer1.Enabled = true;
+            sure = 20;
+            label10.Text = sure.ToString();
+
+            btnA.Enabled = Enabled;
+            btnB.Enabled = Enabled;
+            btnC.Enabled = Enabled;
+            btnD.Enabled = Enabled;
+            btnBasla.Text = "İleri";
+            soruNo++;
+            label1.Text = soruNo.ToString();
+
+            if(soruNo == 1)
+            {
+                richTextBox1.Text = "Mustafa Kemal Atatürk kaç yılında doğmuştur ?";
+                btnA.Text = "1880";
+                btnB.Text = "1991";
+                btnC.Text = "1881";
+                btnD.Text = "1891";
+                label4.Text = "1881";
+            }
+            if (soruNo == 2)
+            {
+                richTextBox1.Text = "Şirinler adlı çizgi filmde hangi adla bilinen şirinin kolunda kırmızı bir kalp vardır?";
+                btnA.Text = "Obur Şirin";
+                btnB.Text = "Şair Şirin";
+                btnC.Text = "Şirine";
+                btnD.Text = "Güçlü Şirin";
+                label4.Text = "Güçlü Şirin";
+            }
+            if (soruNo == 3)
+            {
+                richTextBox1.Text = "Saçını başının üst bölümünde toplayıp dibinden bağlayarak at kuyruğu şeklinde aşağıya sarkıtan biri hangi saç biçimini yapmış olur?";
+                btnA.Text = "Topuz";
+                btnB.Text = "Kakül";
+                btnC.Text = "Rasta";
+                btnD.Text = "Atkuyruğu";
+                label4.Text = "Atkuyruğu";
+            }
+            if (soruNo == 4)
+            {
+                richTextBox1.Text = "Deyime göre, öğrenim görmüş, kültürlü kişiler ne yalamıştır?";
+                btnA.Text = "Dondurma";
+                btnB.Text = "Horoz şekeri";
+                btnC.Text = "Limon";
+                btnD.Text = "Mürekkep";
+                label4.Text = "Mürekkep";
+            }
+            if (soruNo == 5)
+            {
+                richTextBox1.Text = "1+1+1+1+1+1+1+1+1+1+1+1+1+1+1 işleminin sonucu kaçtır?";
+                btnA.Text = "11";
+                btnB.Text = "111";
+                btnC.Text = "5";
+                btnD.Text = "15";
+                label4.Text = "15";
+            }
+            if (soruNo == 6)
+            {
+                richTextBox1.Text = "Kendi adını taşıyan çizgi filmde Kaptan Tsubasa, Japonya'nın hangi spor dalında mücadele eden milli takımlarında boy gösterir?";
+                btnA.Text = "Körling";
+                btnB.Text = "Su topu";
+                btnC.Text = "Futbol";
+                btnD.Text = "Yağlı güreş";
+                label4.Text = "Futbol";
+            }
+            if (soruNo == 7)
+            {
+                richTextBox1.Text = "Hangisi hayvanlar aleminin sürüngenler sınıfından değildir?";
+                btnA.Text = "Yılanlar";
+                btnB.Text = "Kertenkeleler";
+                btnC.Text = "Kurbağalar";
+                btnD.Text = "Kaplumbağalar";
+                label4.Text = "Kaplumbağalar";
+            }
+            if (soruNo == 8)
+            {
+                richTextBox1.Text = "Bulunduğu yerde, orada yaşayanları rahatsız etme pahasına sürekli kalıp oraya yerleşen biri ne yapmış olur?";
+                btnA.Text = "Kreplenmiş";
+                btnB.Text = "Pastalanmış";
+                btnC.Text = "Çöreklenmiş";
+                btnD.Text = "Böreklenmiş";
+                label4.Text = "Çöreklenmiş";
+            }
+            if (soruNo == 9)
+            {
+                richTextBox1.Text = "5 makinenin 5 cihaz üretmesi 5 dakika sürüyorsa 100 makinenin 100 cihaz üretmesi ne kadar sürer?";
+                btnA.Text = "5 dakika";
+                btnB.Text = "25 dakika";
+                btnC.Text = "1 saat";
+                btnD.Text = "1 saat 40 dakika";
+                label4.Text = "1 saat 40 dakika";
+            }
+            if (soruNo == 10)
+            {
+                btnBasla.Visible = false;
+                btnBasla.Enabled = false;
+                richTextBox1.Text = "DUS'a girecek olan biri hangi fakülteden mezun olmuştur?";
+                btnA.Text = "Hukuk";
+                btnB.Text = "Diş Hekimliği";
+                btnC.Text = "Spor Bilimleri";
+                btnD.Text = "Denizcilik";
+                label4.Text = "Diş Hekimliği";                               
+            }
+           
+            
+        }
+
+        private void btnA_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            btnBasla.Enabled = true;
+            btnA.Enabled = false;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            label3.Text = btnA.Text;
+            if (label3.Text == label4.Text)
+            {
+                MessageBox.Show("Cevabınız Doğru");
+                dogruCevap++;
+                label5.Text = dogruCevap.ToString();
+            }
+            else
+            {
+                yanlisCevap++;
+                label7.Text = yanlisCevap.ToString();
+            }
+        }
+
+        private void btnB_Click(object sender, EventArgs e)
+        {
+            btnBasla.Enabled = true;
+            timer1.Enabled = false;
+            btnA.Enabled = false;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            label3.Text = btnB.Text;
+            if (label3.Text == label4.Text)
+            {
+                MessageBox.Show("Cevabınız Doğru");
+                dogruCevap++;
+                label5.Text = dogruCevap.ToString();
+
+            }
+            else
+            {
+                yanlisCevap++;
+                label7.Text = yanlisCevap.ToString();
+            }
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            btnBasla.Enabled = true;
+            timer1.Enabled = false;
+            btnA.Enabled = false;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            label3.Text = btnC.Text;
+            if (label3.Text == label4.Text)
+            {
+                MessageBox.Show("Cevabınız Doğru");
+                dogruCevap++;
+                label5.Text = dogruCevap.ToString();
+
+            }
+            else
+            {
+                yanlisCevap++;
+                label7.Text = yanlisCevap.ToString();
+            }
+        }
+
+        private void btnD_Click(object sender, EventArgs e)
+        {
+            btnBasla.Enabled = true;
+            timer1.Enabled = false;
+            btnA.Enabled = false;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            label3.Text = btnD.Text;
+            if (label3.Text == label4.Text)
+            {
+                MessageBox.Show("Cevabınız Doğru");
+                dogruCevap++;
+                label5.Text = dogruCevap.ToString();
+
+            }
+            else
+            {
+                yanlisCevap++;
+                label7.Text = yanlisCevap.ToString();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sure = Convert.ToInt32(label10.Text);
+
+            sure = sure - 1;
+            label10.Text = sure.ToString();
+            if (sure == 0)
+            {
+                timer1.Enabled = false;
+                yanlisCevap++;
+                label7.Text = yanlisCevap.ToString();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnA.Enabled = false;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+        }
+    }
+}
